81a7bbfc78dd2b871047bd3d5912ca22b4cf7fed
harimmon <143974982+harimmon@users.noreply.github.com>
2025-06-27 21:05:19 +0900

    [feat] 정규 리그 전체 일정 저장 (#5)
    기능
    
    Game(게임 목록) 크롤링 구현
    Game관련 C,S,R 그리고 DTO 구현
    관리자용 Game 저장 호출 구현
    
    비고
    
    예외처리 X
    Optional 처리 X
    필터 처리 X

--- src/main/java/com/example/ballkkaye/_core/util/UtilMapper.java
+++ src/main/java/com/example/ballkkaye/_core/util/UtilMapper.java
@@ -1,5 +1,6 @@
 package com.example.ballkkaye._core.util;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class UtilMapper {
@@ -11,5 +12,28 @@ public class UtilMapper {
 
     public static Integer getTeamId(String shortName) {
         return teamNameToId.get(shortName);
+    }
+
+    private static final Map<String, Integer> stadiumNameToId;
+    
+    static {
+        stadiumNameToId = new HashMap<>();
+        stadiumNameToId.put("잠실", 1);
+        stadiumNameToId.put("고척", 2);
+        stadiumNameToId.put("문학", 3);
+        stadiumNameToId.put("광주", 4);
+        stadiumNameToId.put("대구", 5);
+        stadiumNameToId.put("사직", 6);
+        stadiumNameToId.put("대전(신)", 7);
+        stadiumNameToId.put("창원", 8);
+        stadiumNameToId.put("수원", 9);
+        stadiumNameToId.put("청주", 10);
+        stadiumNameToId.put("울산", 11);
+        stadiumNameToId.put("포항", 12);
+        stadiumNameToId.put("군산", 13);
+    }
+
+    public static Integer getStadiumId(String shortName) {
+        return stadiumNameToId.get(shortName);
     }
 }
--- src/main/java/com/example/ballkkaye/stadium/StadiumRepository.java
+++ src/main/java/com/example/ballkkaye/stadium/StadiumRepository.java
@@ -4,9 +4,13 @@ import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
-
 @RequiredArgsConstructor
 @Repository
 public class StadiumRepository {
+
     private final EntityManager em;
+
+    public Stadium findById(Integer stadiumId) {
+        return em.find(Stadium.class, stadiumId);
+    }
 }
