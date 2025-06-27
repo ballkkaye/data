9df90b9544f0be53e3246e5e061fda891cf54ec1
hahamik <dlapdl0615@gmail.com>
2025-06-27 19:14:30 +0900

    [feat] TeamRecord 저장 (#1)
    기능
    
    TeamRecord (팀 순위) 크롤링 구현
    TeamRecord 관련 C,S,R 그리고 DTO 구현
    관리자용 TeamRecord 저장 호출 구현
    비고
    
    예외처리 X
    Optional 처리 X
    필터 처리 X

--- src/main/java/com/example/ballkkaye/team/record/TeamRecordRequest.java
+++ src/main/java/com/example/ballkkaye/team/record/TeamRecordRequest.java
@@ -1,4 +1,41 @@
 package com.example.ballkkaye.team.record;
 
+import lombok.Data;
+
 public class TeamRecordRequest {
+
+    @Data
+    public static class Dto {
+        private Integer teamId;
+        private Integer teamRank;
+        private Integer totalGame;
+        private Integer winGame;
+        private Integer loseGame;
+        private Integer tieGame;
+        private Double winRate;
+        private Double gap;
+        private String recentTenGames;
+        private String streak;
+        private Double OPS;
+        private Integer R;
+        private Double ERA;
+
+        public Dto(Integer teamId, Integer teamRank, Integer totalGame, Integer winGame, Integer loseGame, Integer tieGame,
+                   Double winRate, Double gap, String recentTenGames, String streak, Double OPS, Integer r, Double ERA) {
+            this.teamId = teamId;
+            this.teamRank = teamRank;
+            this.totalGame = totalGame;
+            this.winGame = winGame;
+            this.loseGame = loseGame;
+            this.tieGame = tieGame;
+            this.winRate = winRate;
+            this.gap = gap;
+            this.recentTenGames = recentTenGames;
+            this.streak = streak;
+            this.OPS = OPS;
+            this.R = r;
+            this.ERA = ERA;
+        }
+    }
 }
+
