ef53536271502ba76c1921982b445570930c09e3
hahamik <dlapdl0615@gmail.com>
2025-06-27 20:57:09 +0900

    [feat] Player 저장 (#4)
    기능
    
    Player (선수 목록) 크롤링 구현
    Player 관련 C,S,R 그리고 DTO 구현
    관리자용 Player 저장 호출 구현
    
    비고
    
    예외처리 X
    Optional 처리 X
    필터 처리 X

--- src/main/java/com/example/ballkkaye/player/PlayerController.java
+++ src/main/java/com/example/ballkkaye/player/PlayerController.java
@@ -1,10 +1,26 @@
 package com.example.ballkkaye.player;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
 public class PlayerController {
     private final PlayerService playerService;
+
+    // 선수 목록 저장 로직
+    // 정기 실행: 매주 일요일 자정
+    @Scheduled(cron = "0 0 0 * * *", zone = "Asia/Seoul")
+    public void saveBot() {
+        // DB에 저장
+        playerService.saveBot();
+    }
+
+    // 관리자용 API
+    @GetMapping("/s/admin/bot/player")
+    public void adminSaveBot() {
+        playerService.saveBot();
+    }
 }
--- src/main/java/com/example/ballkkaye/player/PlayerRepository.java
+++ src/main/java/com/example/ballkkaye/player/PlayerRepository.java
@@ -4,8 +4,17 @@ import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @RequiredArgsConstructor
 @Repository
 public class PlayerRepository {
     private final EntityManager em;
+
+    // 선수 목록 저장(player_tb)
+    public void saveAll(List<Player> players) {
+        for (Player player : players) {
+            em.persist(player);
+        }
+    }
 }
