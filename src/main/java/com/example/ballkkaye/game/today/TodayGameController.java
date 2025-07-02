3a057a77905d129f80643532797ce969364c7d50
harimmon <bhr0502@gmail.com>
2025-07-02 19:10:05 +0900

    오늘의 경기 갱신

--- src/main/java/com/example/ballkkaye/game/today/TodayGameController.java
+++ src/main/java/com/example/ballkkaye/game/today/TodayGameController.java
@@ -3,6 +3,7 @@ package com.example.ballkkaye.game.today;
 import com.example.ballkkaye._core.util.Resp;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +12,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class TodayGameController {
 
     private final TodayGameService todayGameService;
+
+    // 30분마다 자동 실행
+    @Scheduled(cron = "0 0/30 * * * *", zone = "Asia/Seoul")
+    public void scheduledUpdateTodayGames() {
+        todayGameService.updateTodayGames();
+    }
 
     @GetMapping("/api/admin/today-games/init")
     public ResponseEntity<?> initTodayGames() {
