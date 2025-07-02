5b9113d50d60ce59f4e8bf7c4aaa0b5492e19b11
harimmon <bhr0502@gmail.com>
2025-07-02 18:33:52 +0900

    오늘의 경기 갱신

--- src/main/java/com/example/ballkkaye/game/today/TodayGameController.java
+++ src/main/java/com/example/ballkkaye/game/today/TodayGameController.java
@@ -1,6 +1,9 @@
 package com.example.ballkkaye.game.today;
 
+import com.example.ballkkaye._core.util.Resp;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
@@ -9,15 +12,9 @@ public class TodayGameController {
 
     private final TodayGameService todayGameService;
 
-//    @GetMapping("/api/admin/today-games/init")
-//    public ResponseEntity<?> initTodayGames() {
-//        todayGameService.updateTodayGames();
-//        return Resp.ok("오늘의 경기 갱신 완료");
-//    }
-//
-//    @GetMapping("/api/today-games/prediction")
-//    public ResponseEntity<?> getTodayPredictions() {
-//        List<TodayGameResponse.PredictionDTO> respDTO = todayGameService.getTodayGamePredictions();
-//        return Resp.ok(respDTO);
-//    }
+    @GetMapping("/api/admin/today-games/init")
+    public ResponseEntity<?> initTodayGames() {
+        todayGameService.updateTodayGames();
+        return Resp.ok("오늘의 경기 갱신 완료");
+    }
 }
