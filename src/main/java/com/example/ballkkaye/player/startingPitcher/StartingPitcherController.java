b82df5b6f204850697a1d67b2410840c3f7aeaa4
harimmon <bhr0502@gmail.com>
2025-07-09 17:45:11 +0900

    컨트롤러 메서드명, url 수정

--- src/main/java/com/example/ballkkaye/player/startingPitcher/StartingPitcherController.java
+++ src/main/java/com/example/ballkkaye/player/startingPitcher/StartingPitcherController.java
@@ -1,6 +1,7 @@
 package com.example.ballkkaye.player.startingPitcher;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,22 +11,20 @@ public class StartingPitcherController {
     private final StartingPitcherService startingPitcherService;
 
     // ① 월~토 17:00 ~ 23:50 (10분 간격)
-//    @Scheduled(cron = "0 */10 17-23 * * MON-SAT", zone = "Asia/Seoul")
-    public void syncEveningStartingPitchers() {
+    @Scheduled(cron = "0 */10 17-23 * * MON-SAT", zone = "Asia/Seoul")
+    public void scheduledSyncEveningStartingPitchers() {
         startingPitcherService.startingPitchers();
     }
 
     // ② 화~일 00:00 ~ 12:00 (10분 간격) → 전날 밤 작업 이어받음
-//    @Scheduled(cron = "0 */10 0-12 * * TUE-SUN", zone = "Asia/Seoul")
-    public void syncMorningStartingPitchers() {
+    @Scheduled(cron = "0 */10 0-12 * * TUE-SUN", zone = "Asia/Seoul")
+    public void scheduledSyncMorningStartingPitchers() {
         startingPitcherService.startingPitchers();
     }
 
-
-    // 관리자용 - role: admin / bot: 자동화
-    @GetMapping("/admin/bot/starting-pitcher")
-    public String StartingPitchers() {
+    // 관리자용 선발 투수 insert
+    @GetMapping("/s/admin/bot/starting-pitcher")
+    public void adminStartingPitchers() {
         startingPitcherService.startingPitchers();
-        return "크롤링 완료"; // TODO: 관리자 페이지 만들어지면 수정
     }
 }
