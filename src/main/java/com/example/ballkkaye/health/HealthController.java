8adbe99be2ba9d1ef368460902f9c32bf7f7de65
harimmon <143974982+harimmon@users.noreply.github.com>
2025-07-16 17:17:14 +0900

    [feat] ballkkaye-data v-0.0.6 (#24)
    * [fix] Sentry io (#22)
    
    백엔드랑 엔티티 구조 동일하게 변경
    sentry 추가
    프로퍼티 수정 - dev모드는 .env 환경변수 읽고, prod 모드는 등록한 환경변수 읽도록 수정
    
    * .
    
    * .
    
    * .
    
    * [feat] 배포준비 (#23)
    
    * [feat] Docker 설정 파일 (#17)
    
    application.properties 수정
    
    RedisConfig 추가
    
    Docker 설정 파일 추가
    
    * [fix] Redis 제거 & FCM 추가 (#18)
    
    - FCM 추가
    - Redis 제거
    - 서버 실행 이상 없음 확인
    
    * [fix] 크롤링 엔티티와 메인 엔티티 구조 통일 (#19)
    
    크롤링 엔티티와 메인 엔티티 구조 통일
    
    * [feat] Sentry (#20)
    
    Exception 수정
    출력문 -> log로 변경
    500 에러는 sentry
    sentry 키, 기상청 인증키, 파이어베이스 키 .env 파일에서 환경변수 가져오도록 수정함
    
    * [fix] firebase키 base64 디코딩 (#21)
    
    배포 대비하여 키 디코딩 후 환경변수 읽도록 수정함
    
    * CI/CD 하는 중
    
    * dot 처내
    
    ---------
    
    Co-authored-by: parangdajavous <nuit0535@gmail.com>
    Co-authored-by: hahamik <dlapdl0615@gmail.com>
    
    * [feat] 배포준비 (
    
    * 배포 준비
    
    * .
    
    * jar 파일 한개만
    
    * healthCheck
    
    * healthCheck
    
    ---------
    
    Co-authored-by: parangdajavous <nuit0535@gmail.com>
    Co-authored-by: hahamik <dlapdl0615@gmail.com>

--- src/main/java/com/example/ballkkaye/health/HealthController.java
+++ src/main/java/com/example/ballkkaye/health/HealthController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class HealthController {
 
-    @GetMapping("/health")
+    @GetMapping("/")
     public String health() {
         return "ok";
     }
